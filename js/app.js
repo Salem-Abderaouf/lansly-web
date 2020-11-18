3c2f2bb42be974bec23eb304a8a3d4603fae3277
Salem-Abderaouf <salamasalem84@gmail.com>
2020-11-18 11:26:48 +0100

    update nav bar style

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const nav = document.querySelector('nav ul')
+const navList = document.querySelector('#navbar ul')
 const MiniNav = document.getElementsByClassName('mini-navBar')[0]
 const MenuButton = document.getElementsByClassName('nav-menu')[0]
 
@@ -12,11 +12,11 @@ const toggelNavbar = () => {
 }
 const checkWindowWidth = () => {
   if (window.innerWidth < 620) {
-    nav.style.display = "none"
+    navList.style.display = "none"
     MenuButton.style.display = "block"
   }
   else {
-    nav.style.display = "flex"
+    navList.style.display = "flex"
     MenuButton.style.display = "none"
   }
 }
